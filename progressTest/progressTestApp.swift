3de9dcd9b939d81ebb0a6f4e56984c7c838dd2ca
Enzo Luigi Schork <enzo.luigi@havan.com.br>
2024-08-28 16:33:42 -0300

    Initial Commit

---
+++ progressTest/progressTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  progressTestApp.swift
+//  progressTest
+//
+//  Created by Enzo Luigi Schork on 28/08/24.
+//
+
+import SwiftUI
+
+@main
+struct progressTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
